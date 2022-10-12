edd1fd950370cf06e98fda29bd9ed7f100caddf2
Aymeric <aymeric.goudout@ens.uvsq.fr>
2022-10-12 16:24:23 +0200

    toc

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # cybers
+Faire un jeu 2D CyberPunk en Python:
+	-Texturing ==> Sprit
+		-Réaliser un Tileset
+		==> Top-Down et éventuellement platformer
+	-Physique terrain, personnage etc..
+	-Jouer en Multi via le local. (un hebergeur)
+	
+	Lore !!! Inspiré de Cyberpunk et Edgerunner !!!
+Top-Down style monde ouvert limité; gestion aninal-crossing like; expéditions en plateformer
---
+++ main.py
@@ -0,0 +1,52 @@
+import pygame
+
+background_image = "tilesheet/background/nuit-etoile-mont-blanc.jpg"
+HAUTEUR, LARGEUR = 1920, 1080
+
+pygame.init()
+screen = pygame.display.set_mode((HAUTEUR,LARGEUR))
+pygame.display.set_caption("Cyber's")
+background = pygame.image.load(background_image).convert()
+run = True
+taille,x,y = 10,LARGEUR//2,HAUTEUR//2 #Variable statique
+movex,movey,agrandir = 0,0,0 #Variable delta
+while run:
+    for event in pygame.event.get():
+        print(event)
+        if event.type == pygame.QUIT:
+            run = False
+        
+        if event.type == pygame.KEYDOWN: # If clée pressée
+            if event.key == pygame.K_SPACE:
+                agrandir = 1
+            elif event.key == pygame.K_UP:
+                movey = 2
+            elif event.key == pygame.K_DOWN:
+                movey = -2
+            elif event.key == pygame.K_RIGHT:
+                movex = 2
+            elif event.key == pygame.K_LEFT:
+                movex = -2
+
+
+        elif event.type == pygame.KEYUP: # If clée relâchée
+            if event.key == pygame.K_SPACE:
+                agrandir = 0
+            elif event.key == pygame.K_UP:
+                movey = 0
+            elif event.key == pygame.K_DOWN:
+                movey = 0
+            elif event.key == pygame.K_RIGHT:
+                movex = 0
+            elif event.key == pygame.K_LEFT:
+                movex = 0
+    
+    x += movex
+    y += movey
+    taille += agrandir
+    screen.blit(background,(0,0))
+    pygame.draw.circle(background, (90, 50, 250), (x, y), taille)
+    pygame.display.update()
+
+pygame.quit()
+quit()
