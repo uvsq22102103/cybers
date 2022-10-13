afd97ed734b4c9d401ae8fc697b4c93058e381e7
Cyriac <cyriac.thibaudeau@ens.uvsq.fr>
2022-10-13 15:45:14 +0200

    Update menucyriac.py

--- menucyriac.py
+++ menucyriac.py
@@ -1,36 +1,50 @@
 import pygame
 import pygame_menu
 from pygame_menu import themes
+import pygame_widgets
 
+#Ressources################################
+###########################################
+background_image = "tilesheet/background/nuit-etoile-mont-blanc.jpg"
+perso1_image = "tilesheet/perso1/pepe1.png"
 HAUTEUR, LARGEUR = 1920,1080
 
-pygame.init()
-screen = pygame.display.set_mode((HAUTEUR,LARGEUR))
-pygame.display.set_caption("Cyber's")
-
+#Fonctions#################################
+###########################################
 def start_the_game():
     mainmenu._open(loading)
     pygame.time.set_timer(update_loading,30)
 
 def option_menu():
-    pass
+    mainmenu._open(optionsmenu)
 
 
+#Initialisation de Pygame##################
+###########################################
+pygame.init()
+
+screen = pygame.display.set_mode((HAUTEUR,LARGEUR))
+pygame.display.set_caption("Cyber's")
+
+#Menus#####################################
+###########################################
 mainmenu = pygame_menu.Menu("Cyber's",HAUTEUR,LARGEUR,theme = themes.THEME_DARK)
-mainmenu.add.text_input("Name:", default="Entre ton cyber-nom", maxchar=20)
+mainmenu.add.text_input("Pseudo:", default="Entre ton cyber-nom", maxchar=20)
 mainmenu.add.button("Jouer", start_the_game)
 mainmenu.add.button("Options", option_menu)
+arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (100,150))
 
 
+optionsmenu = pygame_menu.Menu("Options",HAUTEUR,LARGEUR,theme = themes.THEME_DARK)
+optionsmenu.add.range_slider("Volume", rangeslider_id = "2", default=100, range_values= (0,100), range_widht= 200, increment= 2)
 
 loading = pygame_menu.Menu("Chargement en cours",HAUTEUR,LARGEUR,theme=themes.THEME_BLUE)
 loading.add.progress_bar("Connection au Cybernet", progressbar_id = "1", default=0, width = 200)
-
-arrow = pygame_menu.widgets.LeftArrowSelection(arrow_size = (100,150))
-
 update_loading = pygame.USEREVENT + 0
-run = True
 
+#Boucle de démarrage#######################
+###########################################
+run = True
 while run:
     events = pygame.event.get()
     for event in events:
@@ -43,6 +57,7 @@ while run:
             if progress.get_value() == 100:
                 pygame.time.set_timer(update_loading,0)
 
+        #Gestion mapping/keys##############
         if event.type == pygame.KEYDOWN: # If clée pressée
             if event.key == pygame.K_ESCAPE:
                 pass
@@ -56,8 +71,6 @@ while run:
                 movex = -2
             elif event.key == pygame.K_ESCAPE:
                 quit()
-
-
         elif event.type == pygame.KEYUP: # If clée relâchée
             if event.key == pygame.K_ESCAPE:
                 run = False
@@ -70,11 +83,13 @@ while run:
             elif event.key == pygame.K_LEFT:
                 movex = 0
 
+    #Affiche le menu choisi###########
     if mainmenu.is_enabled():
         mainmenu.update(events)
         mainmenu.draw(screen)
         if mainmenu.get_current().get_selected_widget():
             arrow.draw(screen, mainmenu.get_current().get_selected_widget())
+    
 
     pygame.display.update()
 
